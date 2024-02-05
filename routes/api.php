8e51653426040a42595cc3f11b7d9ca24262e2e8
Anthony Nwokoma <anthonynwokoma1@gmail.com>
2024-02-05 17:37:53 +0100

    Recieve Data3

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 // Route::middleware('auth:sanctum')->group(function () {
     Route::post('/login', [App\Http\Controllers\TasksController::class, 'login']);
